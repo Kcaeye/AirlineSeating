00adb67fbe3f55d53b801967fcd198d26913de1a
Leo Irakliotis <lgreco@gmail.com>
2020-08-28 21:25:14 -0500

    Updated documentation in realisticName()
    Signed-off-by: Leo Irakliotis <lgreco@gmail.com>

--- src/realisticNameGenerator.java
+++ src/realisticNameGenerator.java
@@ -1,7 +1,18 @@
 /**
  * A class to provide a passenger manifest using realistic
  * first and last names, using popular (ie) frequent first
- * and last names in the US.
+ * and last names in the US. The class comprises three String
+ * arrays with common masculine and feminine first names. In
+ * a tribute to my COMP 170 SP 20 students -- the first class
+ * to go online due to COVID-19, in March 2020 -- I am using
+ * their last names in array lastnames[].
+ *
+ * The class comprises a method that returns a randomly generated
+ * name, with a 50-50 probability for a male or female person.
+ *
+ * To use this class, instantiate it first, then access its
+ * .realisticName() method.
+ *
  */
 
 import java.util.Random;
@@ -89,23 +100,45 @@ public class realisticNameGenerator {
      */
     public String[] realisticName () {
 
+        /**
+         * Sizes of the name arrays. These are necessary and parameters
+         * for Random.nextInt().
+         */
         int lastNamesCount = lastNames.length;
         int maleFirstNamesCount = firstNamesMale.length;
         int femaleFirstNamesCount = firstNameFemale.length;
 
+        /**
+         * A local variable to be used as a coin flip to determine
+         * if the output will be a masculine or feminine first name.
+         */
         int gender;
+
+        /**
+         * Local array with two elements for first and last name,
+         * respectively. This is the item returned by this method.
+         */
         String[] name = new String[2];
+
+        /** Instantiate random number class */
         Random rand = new Random();
 
-        gender = rand.nextInt(2);       // coin toss to determine gender
+        /** Flip a coid; 0 is female; 1 is male */
+        gender = rand.nextInt(2);
 
+        /** Select a last name at random */
         name[1] = lastNames[rand.nextInt(lastNamesCount)];  // last names common to both genders
 
+        /**
+         * Select a first name at random, from the male or female
+         * array, based on the value of the gender "coin flip"
+         */
         if (gender == 0) {                     // female first name
             name[0] = firstNameFemale[rand.nextInt(femaleFirstNamesCount)];
         } else {                               // male first name
             name[0] = firstNamesMale[rand.nextInt(maleFirstNamesCount)];
         }
+
         /* question for class: how to bias towards female or male first names? */
         return name;
     }
